59067bfe59494b5a8f30391d232108961496adaf
notpop <k.tani.free@gmail.com>
2022-09-17 06:57:32 +0900

    test crontab

--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
+	"net/http"
 
+	// "github.com/joho/godotenv"
 	"github.com/line/line-bot-sdk-go/v7/linebot"
 	"github.com/robfig/cron/v3"
 	"tsugumi_bot/weather"
@@ -11,24 +14,36 @@ import (
 
 func main() {
 	cron := cron.New()
-	cron.AddFunc("* * * * *", func() {
-		lineBot, err := linebot.New(
-			os.Getenv("LINE_BOT_CHANNEL_SECRET"),
-			os.Getenv("LINE_BOT_CHANNEL_TOKEN"),
-		)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		result, err := weather.GetWeather()
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		message := linebot.NewTextMessage(result)
-		if _, err := lineBot.BroadcastMessage(message).Do(); err != nil {
-			log.Fatal(err)
-		}
+	// cron.AddFunc("CRON_TZ=Asia/Tokyo 0 7 * * *", func() {
+	cron.AddFunc("@every 30s", func() {
+		fmt.Println("start linebot")
+		broadcastWeather()
+		fmt.Println("end linebot")
 	})
 	cron.Start()
+
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		fmt.Printf(err.Error())
+	}
+}
+
+func broadcastWeather() {
+	// godotenv.Load(".env")
+	lineBot, err := linebot.New(
+		os.Getenv("LINE_BOT_CHANNEL_SECRET"),
+		os.Getenv("LINE_BOT_CHANNEL_TOKEN"),
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	result, err := weather.GetWeather()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	message := linebot.NewTextMessage(result)
+	if _, err := lineBot.BroadcastMessage(message).Do(); err != nil {
+		log.Fatal(err)
+	}
 }
