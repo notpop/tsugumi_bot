753d47ccd33e601808ff9509c95f4ac4ab8d0f6e
notpop <k.tani.free@gmail.com>
2023-01-28 10:39:02 +0900

    add judge message api limit

--- line/line.go
+++ line/line.go
@@ -1,9 +1,20 @@
 package line
 
 import (
+	"encoding/json"
 	"github.com/line/line-bot-sdk-go/v7/linebot"
 	"log"
 	"time"
+	"tsugumi_bot/config"
+	"tsugumi_bot/utils"
+)
+
+const (
+	ERROR_INT64           = 1000000000
+	ERROR_SETTING_INT64   = 999999999
+	MESSAGE_API_V2_URL    = "https://api.line.me/v2/bot/message"
+	CONSUMPTION_END_POINT = "quota/consumption"
+	QUOTA_END_POINT       = "quota"
 )
 
 type Client struct {
@@ -86,4 +97,67 @@ func (client *Client) pushMessage(message string, id string) error {
 	}
 
 	return nil
+}
+
+type MessageLimitResponse struct {
+	TotalUsage int64 `json:"totalUsage"`
+}
+
+func GetMessageApiLimit() (int64, error) {
+	client := utils.New(MESSAGE_API_V2_URL, map[string]string{
+		"Authorization": "Bearer " + config.Config.ChannelToken,
+	})
+	resp, err := client.DoRequest("GET", CONSUMPTION_END_POINT, map[string]string{}, []byte{})
+	if err != nil {
+		log.Println("Get Http Error: ", err)
+		return ERROR_INT64, err
+	}
+
+	var response MessageLimitResponse
+	err = json.Unmarshal(resp, &response)
+	if err != nil {
+		log.Println("JSON Unmarshal error: ", err)
+		return ERROR_INT64, err
+	}
+
+	return response.TotalUsage, nil
+}
+
+type MessageLimitSettingResponse struct {
+	Type  string `json:"type"`
+	Value int64  `json:"value"`
+}
+
+func GetMessageApiLimitSetting() (int64, error) {
+	client := utils.New(MESSAGE_API_V2_URL, map[string]string{
+		"Authorization": "Bearer " + config.Config.ChannelToken,
+	})
+	resp, err := client.DoRequest("GET", QUOTA_END_POINT, map[string]string{}, []byte{})
+	if err != nil {
+		log.Println("Get Http Error: ", err)
+		return ERROR_SETTING_INT64, err
+	}
+
+	var response MessageLimitSettingResponse
+	err = json.Unmarshal(resp, &response)
+	if err != nil {
+		log.Println("JSON Unmarshal error: ", err)
+		return ERROR_SETTING_INT64, err
+	}
+
+	return response.Value, nil
+}
+
+func IsLimit() bool {
+	limit, err := GetMessageApiLimit()
+	if err != nil {
+		return true
+	}
+
+	limitSetting, err := GetMessageApiLimitSetting()
+	if err != nil {
+		return true
+	}
+
+	return limit > limitSetting
 }
--- line/line_test.go
+++ line/line_test.go
@@ -1,1 +1,15 @@
 package line
+
+import (
+	"testing"
+)
+
+func TestIsLimitApiReturnError(t *testing.T) {
+	if ERROR_INT64 > ERROR_SETTING_INT64 {
+		t.Log("checking limit")
+	} else {
+		t.Error("no checking limit")
+	}
+
+	t.Log("TestIsLimitApiReturnError終了")
+}
