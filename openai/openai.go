8e2e4f07e8240c1e0fb0a3948d30a34954d87531
notpop <k.tani.free@gmail.com>
2023-03-03 03:18:03 +0900

    add request record

--- openai/openai.go
+++ openai/openai.go
@@ -58,6 +58,7 @@ func SendQuestion(prompt string) (*ChatGptResponse, error) {
 		MaxToken: DEFAULT_MAX_TOKEN,
 		Messages: []Message{
 			{Role: "system", Content: config.Config.TsugumiSettings},
+			{Role: "system", Content: config.Config.TsugumiSettings2},
 			{Role: "user", Content: prompt},
 		},
 	}
